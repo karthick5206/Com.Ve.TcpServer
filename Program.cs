18494e6e20cb44dd66f1fb5eb9e81c073b4acfb1
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-05-14 14:19:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Com.Ve.TcpServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
