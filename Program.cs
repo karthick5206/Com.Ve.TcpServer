c01a6594524ee56b9c4d21df2b23ba71612aff36
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-05-14 16:03:09 +0530

    Initial Setup

--- Program.cs
+++ Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Net;
+using System.Text;
+using NetCoreServer;
 
-namespace Com.Ve.TcpServer
+namespace Com.Ve.ServerDataReceiver
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            TcpDataReceiver tcpServer = new TcpDataReceiver(IPAddress.Any, 180);
+            tcpServer.Start();
+
+            Console.Read();
+        }
+    }
+
+    public class TcpDataReceiver : TcpServer
+    {
+        public TcpDataReceiver(IPAddress iPAddress, int port) : base(iPAddress, port)
+        {
+
+        }
+
+        public override void OnReceived(byte[] buffer, long offset, long size)
+        {
+            var receivedDataByteArray = new byte[size];
+            Array.Copy(buffer, receivedDataByteArray, size);
+            Console.WriteLine($"{Encoding.UTF8.GetString(receivedDataByteArray)}");
         }
     }
 }
