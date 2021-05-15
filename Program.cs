af777e92156f84b73b531de7906029df62f20daf
karthik5206 <47052861+karthik5206@users.noreply.github.com>
2021-05-15 18:52:53 +0530

    Included acknowledgement

--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.IO;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using Com.Ve.Parser;
+using Com.Ve.Parser.Utilities;
 using NetCoreServer;
 using Newtonsoft.Json;
 
@@ -18,7 +20,7 @@ namespace Com.Ve.ServerDataReceiver
 
             foreach (var parser in parsers.Parser)
             {
-                TcpDataReceiver tcpServer = new TcpDataReceiver(IPAddress.Any, parser.Port);
+                TcpDataReceiver tcpServer = new TcpDataReceiver(parser);
                 tcpServer.Start();
                 tcpServer.OptionReuseAddress = true;
 
@@ -38,36 +40,83 @@ namespace Com.Ve.ServerDataReceiver
     {
         public string ParserName { get; set; }
         public ParserType ParserType { get; set; }
+        public ParserDataType ParserDataType { get; set; }
         public int Port { get; set; }
         public string RedirectIp { get; set; }
         public int RedirectPort { get; set; }
+        public bool IsAknowledge { get; set; }
     }
 
-    public enum ParserType
+    public enum ParserDataType
     {
         String = 0,
         Hex
     }
 
+    public enum ParserType
+    {
+        Concox = 0,
+        Teltonika,
+        ITraingle
+    }
+
     public class TcpDataReceiver : TcpServer
     {
-        public TcpDataReceiver(IPAddress iPAddress, int port) : base(iPAddress, port)
+        Parser ParserInfo { get; set; }
+        public string IMEI { get; set; }
+        public TcpDataReceiver(Parser parser) : base(IPAddress.Any, parser.Port)
         {
-
+            ParserInfo = parser;
         }
 
         public override void OnReceived(TcpSession tcpSession, byte[] buffer, long offset, long size)
         {
             var receivedDataByteArray = new byte[size];
             Array.Copy(buffer, receivedDataByteArray, size);
-            var receivedData = Encoding.UTF8.GetString(receivedDataByteArray);
+            var receivedData = ParserInfo.ParserDataType == ParserDataType.Hex
+                ? SplitData.GetHexValueFromByteArray(receivedDataByteArray)
+                : Encoding.UTF8.GetString(receivedDataByteArray);
             Console.WriteLine($"{receivedData}");
+            if (ParserInfo.IsAknowledge)
+            {
+                SendAcknowledgeMent(tcpSession, receivedData);
+            }
             RavenDB.RavenDbConnector.Add(new RavenDB.GpsData { Data = receivedData });
         }
 
         protected override void OnError(SocketError error)
         {
             Console.WriteLine($"Server caught an error with code {error}");
+        }
+
+        public void SendAcknowledgeMent(TcpSession tcpSession, string message)
+        {
+            switch (ParserInfo.ParserType)
+            {
+                case ParserType.Concox:
+                    SendAcknowledgeMentToDevice(tcpSession, ConcoxParser.BuildAcknowledgeMessage(message, out string IMEI));
+                    this.IMEI = IMEI;
+                    break;
+                case ParserType.Teltonika:
+                    SendAcknowledgeMentToDevice(tcpSession, TeltonikaParser.BuildAcknowledgeMessage(message));
+                    break;
+            }
+        }
+
+        public void SendAcknowledgeMentToDevice(TcpSession tcpSession, string msg)
+        {
+            try
+            {
+                Console.WriteLine("Acknowledgement message  : " + msg);
+                if (tcpSession != null)
+                {
+                    tcpSession.Send(SplitData.StringToByteArray(msg.Trim()));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"public void SendAcknowledgeMent(Socket _socket) {ex}");
+            }
         }
     }
 }
